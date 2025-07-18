1ef0f4d04d76b03e1ff8027eae59fc5ec56d1854
front-chan <chan.jor.zz@gmail.com>
2025-07-18 23:36:25 +0900

    loader function

--- app/features/products/pages/products-page.tsx
+++ app/features/products/pages/products-page.tsx
@@ -1,23 +1,7 @@
-import type { Route } from "../../../+types/features/products/pages/products-page";
-import type { MetaFunction } from "@react-router/types";
+import { redirect } from "react-router";
 
-export function meta(): MetaFunction {
-  return [
-    { title: "Products | ProductHunt Clone" },
-    { naem: "description", content: "Browse all products" },
-  ];
-}
-
-export function loader({ request }: Route.LoaderArgs) {
-  return {
-    products: [],
-  };
-}
-
-export default function ProductsPage({ loaderData }: Route.ComponentProps) {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Products</h1>
-    </div>
-  );
+export function loader() {
+  return redirect("/products/leaderboards");
+  // json 파일 생성 가능
+  // return Response.json({hello: "world"});
 }
